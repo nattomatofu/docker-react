800dc2609c3726504bb305c017642d505e1f926b
t-fujii <taka1211312@gmail.com>
2023-09-13 22:22:27 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>みんな、natto memoブログ見てね</p>
+                <p>みんな、natto memoブログ見てーーーね</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
